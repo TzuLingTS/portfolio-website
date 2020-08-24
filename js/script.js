6b68ab8df474d88ba3a779634f406a48b43d47c7
TzuLingTS <53162912+TzuLingTS@users.noreply.github.com>
2020-08-24 17:43:05 +0800

    Created hamburger menu for mobile

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 var message = "Welcome to my portfolio!";
 console.log(message);
+
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
