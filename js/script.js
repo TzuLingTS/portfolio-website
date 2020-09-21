c58c3b4d8a266cdf93388f03913c378e2c0df9b4
TzuLingTS <53162912+TzuLingTS@users.noreply.github.com>
2020-09-21 14:33:27 +0800

    Added tooltip and "copy to clipboard" on footer

--- js/script.js
+++ js/script.js
@@ -20,8 +20,7 @@ $('a[href*="#"]')
   .click(function(event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
@@ -36,13 +35,12 @@ $('a[href*="#"]')
         }, 1000, function() {
           // Callback after animation
           // Must change focus!
-          var $target = $(target)
-;
+          var $target = $(target);
           $target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
           };
         });
@@ -51,12 +49,14 @@ $('a[href*="#"]')
   });
 
 //Copy Email
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
+function copyElementText() {
+  var text = document.getElementById("emailCopy").innerText;
+  var elem = document.createElement("textarea");
+  document.body.appendChild(elem);
+  elem.value = text;
+  elem.select();
   document.execCommand("copy");
-  $temp.remove();
+  document.body.removeChild(elem);
 }
 
 
